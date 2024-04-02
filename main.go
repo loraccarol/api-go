9ff89d7b0a5dd2dd92030af87bfa6def839809ac
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2024-04-01 22:27:15 -0300

    codigo para converter moeda em GO

--- main.go
+++ main.go
@@ -2,32 +2,95 @@ package main
 
 import (
 	"encoding/json"
-	"log"
-	"math/rand"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
-const creditScoreMin = 500
-const creditScoreMax = 900
-
-type credit_rating struct {
-	CreditRating int `json:"credit_rating"`
+type CurrencyResponse struct {
+	USD string `json:"dolar"`
+	EUR string `json:"euro"`
 }
 
-func getCreditScore(w http.ResponseWriter, r *http.Request) {
-	var creditRating = credit_rating{
-		CreditRating: (rand.Intn(creditScoreMax-creditScoreMin) + creditScoreMin),
+type CurrencyData struct {
+	Code string `json:"code"`
+	Bid  string `json:"bid"`
+}
+
+func getCurrencyData() (usdValue, eurValue float64, err error) {
+	url := "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL"
+
+	response, err := http.Get(url)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Erro ao fazer a solicitação HTTP: %v", err)
+	}
+	defer response.Body.Close()
+
+	var data map[string]CurrencyData
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Erro ao analisar a resposta JSON: %v", err)
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(creditRating)
+	usdValue, err = strconv.ParseFloat(data["USDBRL"].Bid, 64)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Erro ao converter valor de USD para float64: %v", err)
+	}
+
+	eurValue, err = strconv.ParseFloat(data["EURBRL"].Bid, 64)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Erro ao converter valor de EUR para float64: %v", err)
+	}
+
+	return usdValue, eurValue, nil
 }
 
-func handleRequests() {
-	http.Handle("/creditscore", http.HandlerFunc(getCreditScore))
-	log.Fatal(http.ListenAndServe(":8123", nil))
+func currencyHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Erro ao ler o corpo da solicitação", http.StatusBadRequest)
+		return
+	}
+
+	var requestBody map[string]string
+	err = json.Unmarshal(body, &requestBody)
+	if err != nil {
+		http.Error(w, "Erro ao analisar o JSON da solicitação", http.StatusBadRequest)
+		return
+	}
+
+	usdValue, eurValue, err := getCurrencyData()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	brlValue, err := strconv.ParseFloat(requestBody["real"], 64)
+	if err != nil {
+		http.Error(w, "Valor em BRL inválido", http.StatusBadRequest)
+		return
+	}
+
+	usdAmount := brlValue * usdValue
+	eurAmount := brlValue * eurValue
+
+	currencyResponse := CurrencyResponse{
+		USD: fmt.Sprintf("%.2f", usdAmount),
+		EUR: fmt.Sprintf("%.2f", eurAmount),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(currencyResponse)
 }
 
 func main() {
-	handleRequests()
+	http.HandleFunc("/convertamoeda", currencyHandler)
+	http.ListenAndServe(":8123", nil)
 }
